0fd05568a9cfaad026e93f162bb3b0b20126ce7c
rstarkov <sourcecode@roman.st>
2016-10-11 20:13:37 +0100

    Blank project plus FFmpeg

---
+++ BasicFFEncode/BasicFFEncode.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "BasicFFEncode.h"
+
+BasicFFEncode::BasicFFEncoder::BasicFFEncoder(String ^ filename)
+{
+    throw gcnew System::NotImplementedException();
+}
+
+BasicFFEncode::BasicFFEncoder::~BasicFFEncoder()
+{
+    throw gcnew System::NotImplementedException();
+}
---
+++ BasicFFEncode/BasicFFEncode.h
@@ -0,0 +1,13 @@
+#pragma once
+
+using namespace System;
+
+namespace BasicFFEncode
+{
+    public ref class BasicFFEncoder
+    {
+    public:
+        BasicFFEncoder(String ^ filename);
+        ~BasicFFEncoder();
+    };
+}
